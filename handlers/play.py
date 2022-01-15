82379b9b78a43e80e7408563cdeb5ffccf1fbb06
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-15 13:35:09 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -323,7 +323,7 @@ async def play(_, message: Message):
         await message.reply_photo(
             photo="final.png",
             reply_markup=keyboard,
-            caption="» ɴᴏᴡ sᴛᴀʀᴛᴇᴅ ᴘʟᴀʏɪɴɢ`{}`ʙᴀʙʏ".format(
+            caption="» sᴛᴀʀᴛᴇᴅ ᴘʟᴀʏɪɴɢ sᴏᴍᴇᴛʜɪɴɢ ɪɴ​ `{}`ʙᴀʙʏ".format(
         message.chat.title
         ), )
 
